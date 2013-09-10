4e7c61d3236414bd96e0da029f060f4f51754dd9
Yoji Itoh <yoji.itoh@klip.co.jp>
2013-09-10 22:30:45 +0900

    add numberFormat-1.0.1.js

---
+++ numberFormat-1.0.1.js
@@ -0,0 +1,16 @@
+/**
+ * numberFormat.js
+ * https://github.com/yangci/numberFormat.js
+ * 
+ * Copyright (c) 2013 KLIP LLC
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ * @Author: Yoji Itoh (yoji.itoh@klip.co.jp)
+ * @Version: 1.0.1
+ **/
+
+Object.prototype.numberFormat = function() {
+	return this.toString().replace(/([0-9]+?)(?=(?:[0-9]{3})+$)/g, '$1,');
+}
